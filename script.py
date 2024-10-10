4a45cf8fceb1320d4e3edffdc04b3ad9198ba9ec
Ricky Roberts <Richard.robertsf@gmail.com>
2024-10-10 11:34:42 -0400

    Finished Dockerfile and script

--- script.py
+++ script.py
@@ -1,12 +1,13 @@
-import socket
 from collections import Counter
+import socket
+import os
 
-# Count the total number of words in each text file located at /home/data
+# Count the total number of words in a file
 def numWordsInFile(fileName):
   fileObject = open(fileName, "r")
   return len(fileObject.read().split())
 
-# Identify the top 3 most frequent words and their respective counts in IF.txt
+# Identify the top 3 most frequent words and their respective counts in a file
 def top3WordsInFile(fileName):
   fileObject = open(fileName, "r")
   words = fileObject.read().lower().split()
@@ -42,50 +43,63 @@ def handleContractions(fileName):
       noContractionsFile.write(contractions.get(word) + " \n")
     else:
       noContractionsFile.write(word + " \n")
-      
-  noContractionsFile.close()
   
-  return top3WordsInFile(noContractions)
+  noContractionsFile.close()
+  frequencies = top3WordsInFile(noContractions)
+  os.remove(noContractions)
+  return frequencies
 
 # Determine the IP address of the machine running the container
 def machineIP():
   return socket.gethostbyname(socket.gethostname())
 
-# Write the results to a text file at /home/data/output/result.txt. When the container is 
-# executed, it should print the contents of result.txt to the console before exiting.
-def writeResultsToOutput():
-  return
+# print all results to command line
+def printFromResult():
+  file = open("output/result.txt", "r")
+  print(file.read())
+  file.close()
 
-
-if __name__ == "__main__":
+# run all actions for assignment
+def scriptActions():
   # open text files to read from
   IFtxt = "IF.txt"
   ARUTWtxt = "AlwaysRememberUsThisWay.txt"
   
   # Count the total number of words in each text file located at /home/data
   wordsIF = numWordsInFile(IFtxt)
-  print(wordsIF)
   wordsARUTW = numWordsInFile(ARUTWtxt)
-  print(wordsARUTW)
   
   # Calculate the grand total of words across both files.
   totalWords = wordsIF + wordsARUTW
-  print(totalWords)
   
   # Identify the top 3 most frequent words and their respective counts in IF.txt
   top3FreqIF = top3WordsInFile(IFtxt)
-  print(top3FreqIF)
-  # top3FreqARUTW = top3WordsInFile(ARUTWtxt)
-  # print(top3FreqARUTW)
   
-  # Handle contractions (e.g., I'm, can't, don't) by splitting them into individual words,
-  # then find the top 3 most frequent words and their respective counts in 
-  # AlwaysRememberUsThisWay.txt 
-  # top3FreqNoContractions = handleContractions(IFtxt)
-  # print(top3FreqNoContractions)
+  # remove contractins and get the 3 most frequent words in the file
   top3FreqNoContractions = handleContractions(ARUTWtxt)
-  print(top3FreqNoContractions)
   
-  # Determine the IP address of the machine running the container
+  # Get the IP address of the machine running the container
   ip_address = machineIP()
-  print(ip_address)
+  
+  # Write the results to a text file at /home/data/output/result.txt.
+  if (not os.path.exists("output")):
+    os.mkdir("output")
+  result = open("output/result.txt", "w")
+  
+  result.write(f"There are {wordsIF} words in IF.txt\n")
+  result.write(f"There are {wordsARUTW} words in IF.txt\n")
+  result.write(f"There are {totalWords} words in the two files combined\n")
+  result.write("\nThe top 3 most frequent words in IF.txt:\n")
+  for word, count in top3FreqIF:
+    result.write(f"{word}: {count}\n")
+  result.write("\nThe top 3 most frequent words in AlwaysRememberUsThisWay.txt (CONTRACTIONS REMOVED):\n")
+  for word, count in top3FreqNoContractions:
+    result.write(f"{word}: {count}\n")
+  result.write(f"\nThe IP address of the machine is: {ip_address}\n")
+  
+  result.close()
+  
+  printFromResult()
+
+if __name__ == "__main__":
+  scriptActions()
